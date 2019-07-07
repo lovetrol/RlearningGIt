ee31c73a3b9265da21b112192a8a845a5008d273
Martin Reyes <marthueeeinxs@Martins-MacBook-Air.local>
2019-07-06 22:38:41 -0500

    Primer commit

---
+++ firstProyect/firstProyect/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  firstProyect
+//
+//  Created by Martin Reyes on 7/6/19.
+//  Copyright © 2019 Martin Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeMays(_ sender: UIButton) {
+        
+    }
+    
+}
+
