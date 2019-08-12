ddc4a875e9e7b3c8193136cfc9f4416f0cd1c269
Martho <martho.78.reyes@gmail.com>
2019-08-11 23:22:56 -0500

    Create Project Flappy Bird No images and no code

---
+++ FlappySwift/FlappySwift/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  FlappySwift
+//
+//  Created by Martin Reyes on 8/11/19.
+//  Copyright © 2019 Martin Reyes. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+   
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
